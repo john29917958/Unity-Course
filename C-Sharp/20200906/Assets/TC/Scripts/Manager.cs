2c28db7bbee16ffa3239cf7a4259b2c73ccdbb39
john29917858 <john29917958@gmail.com>
2020-09-06 14:43:29 +0800

    Updates gitignore

---
+++ C-Sharp/20200906/Assets/TC/Scripts/Manager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace tc
+{
+	public class Manager : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+
+}
