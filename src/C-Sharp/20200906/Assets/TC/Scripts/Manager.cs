3217af90705ea48da63e22fd41e12171cc482a8d
john29917858 <john29917958@gmail.com>
2020-09-06 15:14:54 +0800

    Adds start and update log

--- src/C-Sharp/20200906/Assets/TC/Scripts/Manager.cs
+++ src/C-Sharp/20200906/Assets/TC/Scripts/Manager.cs
@@ -4,17 +4,16 @@ namespace tc
 {
 	public class Manager : MonoBehaviour
 	{
-
 		// Use this for initialization
 		void Start()
 		{
-			Debug.Log("Hello World!");
+			Debug.Log("Program starts");
 		}
 
 		// Update is called once per frame
 		void Update()
 		{
-
+			Debug.Log("Program updates");
 		}
 	}
 
