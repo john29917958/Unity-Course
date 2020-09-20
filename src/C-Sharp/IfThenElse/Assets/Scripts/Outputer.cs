2356e311afc5a1013ab09eeee5f0c9619a17d6ea
Tzu-Chao Wang <john29917958@gmail.com>
2020-09-20 15:22:25 +0800

    Completes normal quiz

---
+++ src/C-Sharp/IfThenElse/Assets/Scripts/Outputer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Outputer : MonoBehaviour {
+	public Text Output;
+	public int UpperBound = 10;
+	public int LowerBound = 0;
+	public int Index = 0;
+	public bool ShouldCountDownward = false;
+
+	private bool _isIncreasing = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Output.text = Index.ToString();
+
+		if (Index < UpperBound && _isIncreasing)
+		{
+			Index += 1;
+			if (Index == UpperBound) _isIncreasing = false;
+		}
+		else if (Index > LowerBound && ShouldCountDownward)
+		{
+			Index -= 1;
+		}
+	}
+}
