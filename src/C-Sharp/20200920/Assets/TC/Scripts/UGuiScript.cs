498971c59bd7d56853292e06d5d367066fb16d2d
Tzu-Chao Wang <john29917958@gmail.com>
2020-09-20 10:01:16 +0800

    Creates basic UI elements

--- src/C-Sharp/20200920/Assets/TC/Scripts/UGuiScript.cs
+++ src/C-Sharp/20200920/Assets/TC/Scripts/UGuiScript.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace TC
 {
 	public class UGUIScript : MonoBehaviour
 	{
+		public Button Button;
+		public Image Image;
+		public Text Text;
+		public InputField Input;
 
 		// Use this for initialization
 		void Start()
