f6a1c63b3798eafa3974a6937576855786105339
Tzu-Chao Wang <john29917958@gmail.com>
2020-09-20 16:32:26 +0800

    Completes hard quiz

---
+++ src/C-Sharp/IfThenElse/Assets/Scripts/AdvancedScoreJudgingSystem.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdvancedScoreJudgingSystem : MonoBehaviour {
+	public Subject[] Subjects;
+	public Text ResultText;
+
+	public void Judge()
+	{
+		Subject mainSubject = null;
+
+		foreach (Subject subject in Subjects)
+		{
+			if (subject.IsMainSubject)
+			{
+				mainSubject = subject;
+			}
+		}
+
+		string judgeReuslt = "加權前, 組別 - " + mainSubject.Name + ", 分數 - ";
+		List<string> scoreResults = new List<string>();
+		foreach (Subject subject in Subjects)
+		{
+			try
+			{
+				scoreResults.Add(subject.Name + " : " + subject.Score + "(" + (IsPass(subject) ? "及格" : "不及格") + ")");
+			}
+			catch(Exception)
+			{
+				ResultText.text = subject.Name + "分數格式輸入錯誤！";
+				return;
+			}
+		}
+		judgeReuslt += string.Join(", ", scoreResults.ToArray());
+
+		string weightedJudgeResult = "加權後, 組別 - " + mainSubject.Name + ", 分數 - ";
+		scoreResults.Clear();
+		foreach (Subject subject in Subjects)
+		{
+			try
+			{
+				scoreResults.Add(subject.Name + " : " + subject.WeightedScore + "(" + (IsPass(subject, true) ? "及格" : "不及格") + ")");
+			}
+			catch (Exception)
+			{
+				ResultText.text = subject.Name + "分數格式輸入錯誤！";
+				return;
+			}
+		}
+		weightedJudgeResult += string.Join(", ", scoreResults.ToArray());
+
+		ResultText.text = judgeReuslt + Environment.NewLine + weightedJudgeResult;
+	}
+
+	private bool IsPass(Subject subject, bool isWeighted = false)
+	{
+		double score = subject.IsMainSubject ? subject.Score : subject.WeightedScore;
+
+		return score >= 60;
+	}
+}
--- src/C-Sharp/IfThenElse/Assets/Scripts/Outputer.cs
+++ src/C-Sharp/IfThenElse/Assets/Scripts/Outputer.cs
@@ -9,11 +9,6 @@ public class Outputer : MonoBehaviour {
 	public bool ShouldCountDownward = false;
 
 	private bool _isIncreasing = true;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
