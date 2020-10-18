1ebe42de6eb618164d0d0310093ceca7839e5b98
Tzu-Chao, Wang <john29917958@gmail.com>
2020-10-18 14:43:35 +0800

    Adds for loop project

---
+++ src/C-Sharp/ForLoop/Assets/TC/Scripts/Normal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+
+public class Normal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		PrintQuiz1Result();
+		PrintQuiz2Result();
+		PrintOddNumbers();
+		Print99MultiplyTable();
+	}
+
+	private void PrintQuiz1Result()
+	{
+		string message = string.Empty;
+		for (int i = 0; i < 10; i++)
+		{
+			message += i.ToString() + ',';
+		}
+		message = message.TrimEnd(',');
+		Debug.Log(message);
+	}
+
+	private void PrintQuiz2Result()
+	{
+		string message = string.Empty;
+		for (int i = 9; i >= 0; i--)
+		{
+			message += i.ToString() + ',';
+		}
+		message = message.TrimEnd(',');
+		Debug.Log(message);
+	}
+
+	private void PrintOddNumbers()
+	{
+		string message = string.Empty;
+		for (int i = 0; i < 16; i++)
+		{
+			if (i % 2 != 0)
+			{
+				message += i.ToString() + ',';
+			}
+		}
+		message = message.TrimEnd(',');
+		Debug.Log(message);
+	}
+
+	private void Print99MultiplyTable()
+	{
+		string message = string.Empty;
+		for (int i = 1; i < 10; i++)
+		{
+			for (int j = 1; j < 10; j++)
+			{
+				message = string.Format("{0}x{1}={2}", i, j, i * j);
+				Debug.Log(message);
+			}
+		}		
+	}
+}
---
+++ src/C-Sharp/ForLoop/Assets/TC/Scripts/Simple.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Simple : MonoBehaviour {
+	private List<int> _list = new List<int>();
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 1; i <= 10; i++)
+		{
+			_list.Add(i);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			PrintList();
+		}
+		else if (Input.GetKeyDown(KeyCode.D))
+		{
+			_list.Clear();
+		}
+		else
+		{
+			// Do nothing.
+		}
+	}
+
+	private void PrintList()
+	{
+		string message = string.Empty;
+
+		foreach (int i in _list)
+		{
+			message += i + ",";
+		}
+
+		message = message.TrimEnd(',');
+
+		Debug.Log(message);
+	}
+}
