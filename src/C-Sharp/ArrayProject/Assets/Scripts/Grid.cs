49f20ee63e66955aee55a0b618c5d6b2b2c388d9
Tzu-Chao, Wang <john29917958@gmail.com>
2020-10-18 13:58:50 +0800

    Completes nine-grid game

---
+++ src/C-Sharp/ArrayProject/Assets/Scripts/Grid.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Grid : MonoBehaviour {
+	public Text Text;
+	public Button Button;
+	public NineGrid NineGrid;
+
+	public int Row;
+	public int Col;
+
+	public void OnClicked()
+	{
+		Text.text = NineGrid.PlayerIcon;
+		Button.interactable = false;
+
+		NineGrid.SetGridValue(Row, Col);		
+	}
+}
