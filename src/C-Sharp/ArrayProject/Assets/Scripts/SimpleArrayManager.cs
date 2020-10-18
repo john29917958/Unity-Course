29046e1980e159f5a6ab3e4a94aa033dead05df8
Tzu-Chao, Wang <john29917958@gmail.com>
2020-10-18 12:15:47 +0800

    Adds simple array project

---
+++ src/C-Sharp/ArrayProject/Assets/Scripts/SimpleArrayManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleArrayManager : MonoBehaviour {
+	public Text Message;
+	private List<string> _dynamicArray;
+
+	// Use this for initialization
+	void Start () {
+		_dynamicArray = new List<string>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			_dynamicArray.Clear();
+			_dynamicArray.Add("A");
+			_dynamicArray.Add("B");
+			_dynamicArray.Add("C");
+		}
+		else if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Message.text = string.Join(", ", _dynamicArray.ToArray());
+		}
+		else
+		{
+			// Do nothing.
+		}
+	}
+}
