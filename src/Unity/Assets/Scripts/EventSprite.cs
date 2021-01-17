52e959ad6250131604190615e11713d014c444e0
Tzu-Chao, Wang <john29917958@gmail.com>
2021-01-17 16:17:05 +0800

    Adds event trigger sample code

---
+++ src/Unity/Assets/Scripts/EventSprite.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventSprite : MonoBehaviour, IPointerDownHandler
+{
+    public void OnPointerEnter()
+    {
+        Debug.Log("Mouse entered!");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("I'm clicked!");
+    }    
+}
