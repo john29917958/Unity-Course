1e5d6159505ce3337fa2819af571cdd593dce3bb
Tzu-Chao Wang <john29917958@gmail.com>
2021-02-07 11:38:49 +0800

    Updates RawImage scene

---
+++ src/Unity/Animator/Assets/Scripts/Character.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+	[SerializeField]
+	private Animator Animator;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Q))
+        {
+			Animator.SetTrigger("SayHiTrigger");
+        }
+	}
+}
