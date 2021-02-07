3b33ab69c96ee2779fd97a1b1e173c745cc0654b
Tzu-Chao Wang <john29917958@gmail.com>
2021-02-07 14:06:35 +0800

    Adds scriptable object scene

---
+++ src/Unity/Basics/Assets/Scripts/StateSO.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "State", menuName = "TC/State", order = 1)]
+public class StateSO : ScriptableObject
+{
+	public string StateName;
+
+	public StateSO[] NextStates;
+
+	[MenuItem("TC/Create Example State")]
+	public static void CreateExampleAssetInstance()
+    {
+        StateSO exampleAsset = CreateInstance<StateSO>();
+		AssetDatabase.CreateAsset(exampleAsset, "Assets/ScriptableObjects/ExampleState.asset");
+		AssetDatabase.Refresh();
+    }
+}
