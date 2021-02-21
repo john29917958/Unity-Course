929b063f3e98a3c3dc2f30ad2c5eb7bbfe3e74f5
Tzu-Chao Wang <john29917958@gmail.com>
2021-02-21 14:11:50 +0800

    Refactor

---
+++ src/Unity/Raycast/Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public float Speed = 5;
+	public float RotationSpeed = 40;
+
+	// Update is called once per frame
+	private void FixedUpdate ()
+	{
+		if (Input.GetKey(KeyCode.W))
+		{
+			Vector3 direction = transform.TransformDirection(Vector3.forward);
+			Vector3 movement = direction * Speed * Time.fixedDeltaTime;
+			transform.position += movement;
+		}
+		else if (Input.GetKey(KeyCode.S))
+		{
+			Vector3 direction = transform.TransformDirection(Vector3.back);
+			Vector3 movement = direction * Speed * Time.fixedDeltaTime;
+			transform.position += movement;
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			Vector3 direction = transform.TransformDirection(Vector3.down);
+			Vector3 rotation = direction * RotationSpeed * Time.fixedDeltaTime;
+
+			transform.eulerAngles += rotation;
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			Vector3 direction = transform.TransformDirection(Vector3.up);
+			Vector3 rotation = direction * RotationSpeed * Time.fixedDeltaTime;
+
+			transform.eulerAngles += rotation;
+		}
+	}
+}
--- src/Unity/Raycast/Assets/Scripts/RayDetector.cs
+++ src/Unity/Raycast/Assets/Scripts/RayDetector.cs
@@ -3,25 +3,12 @@
 public class RayDetector : MonoBehaviour {
 	public LayerMask LayerMask;
 	public float RayDistance = 5;
-	public float Speed = 5;
-	public float RotationSpeed = 40;
-
-	// Use this for initialization
-	private void Start ()
-	{
-		
-	}
 	
 	// Update is called once per frame
 	private void Update ()
 	{
 		DetectRayCollision();
 	}
-
-	private void FixedUpdate()
-    {
-		UpdateMovement();
-    }
 
 	private void DetectRayCollision()
     {
@@ -32,7 +19,13 @@ public class RayDetector : MonoBehaviour {
 
 		foreach (RaycastHit hitInfo in Physics.RaycastAll(ray, RayDistance, LayerMask))
 		{
-			Debug.Log(string.Format("Hit \"{0}\" at {1}. Hit point: {2}", hitInfo.transform.name, hitInfo.transform.position, hitInfo.point));
+			Debug.Log(string.Format("Hit \"{0}\" at {1}. Hit point (world): {2}, hit point (screen): {3}, hit point (viewport): {4}.",
+				hitInfo.transform.name,
+				hitInfo.transform.position,
+				hitInfo.point,
+				Camera.main.WorldToScreenPoint(hitInfo.point),
+				Camera.main.WorldToViewportPoint(hitInfo.point)
+			));
 		}
 
 		DrawRay(ray);
@@ -41,36 +34,5 @@ public class RayDetector : MonoBehaviour {
 	private void DrawRay(Ray ray)
     {
 		Debug.DrawRay(ray.origin, ray.direction * RayDistance, Color.red);
-    }
-
-	private void UpdateMovement()
-    {
-		if (Input.GetKey(KeyCode.UpArrow))
-        {
-			Vector3 direction = transform.TransformDirection(Vector3.forward);
-			Vector3 movement = direction * Speed * Time.fixedDeltaTime;
-			transform.position += movement;
-		}
-		else if (Input.GetKey(KeyCode.DownArrow))
-		{
-			Vector3 direction = transform.TransformDirection(Vector3.back);
-			Vector3 movement = direction * Speed * Time.fixedDeltaTime;
-			transform.position += movement;
-		}
-
-		if (Input.GetKey(KeyCode.LeftArrow))
-        {
-			Vector3 direction = transform.TransformDirection(Vector3.down);
-			Vector3 rotation = direction * RotationSpeed * Time.fixedDeltaTime;
-
-			transform.eulerAngles += rotation;
-        }
-		else if (Input.GetKey(KeyCode.RightArrow))
-        {
-			Vector3 direction = transform.TransformDirection(Vector3.up);
-			Vector3 rotation = direction * RotationSpeed * Time.fixedDeltaTime;
-
-			transform.eulerAngles += rotation;
-		}
     }
 }
