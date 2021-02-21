36f497c5e2c0d23a896c42095a599234e34807c6
Tzu-Chao Wang <john29917958@gmail.com>
2021-02-21 11:38:32 +0800

    Adds movement to object

--- src/Unity/Raycast/Assets/Scripts/RayDetector.cs
+++ src/Unity/Raycast/Assets/Scripts/RayDetector.cs
@@ -3,6 +3,7 @@
 public class RayDetector : MonoBehaviour {
 	public LayerMask LayerMask;
 	public float RayDistance = 5;
+	public float Speed = 5;
 
 	// Use this for initialization
 	private void Start ()
@@ -13,12 +14,21 @@ public class RayDetector : MonoBehaviour {
 	// Update is called once per frame
 	private void Update ()
 	{
+		DetectRayCollision();
+	}
+
+	private void FixedUpdate()
+    {
+		UpdateMovement();
+    }
+
+	private void DetectRayCollision()
+    {
 		Vector3 origin = transform.position;
 		Vector3 direction = transform.TransformDirection(Vector3.forward);
 		Ray ray = new Ray(origin, direction);
 
-		RaycastHit hitInfo;
-		if (Physics.Raycast(ray, out hitInfo, RayDistance, LayerMask))
+		foreach (RaycastHit hitInfo in Physics.RaycastAll(ray, RayDistance, LayerMask))
 		{
 			Debug.Log(string.Format("Hit \"{0}\" at {1}. Hit point: {2}", hitInfo.transform.name, hitInfo.transform.position, hitInfo.point));
 		}
@@ -29,5 +39,27 @@ public class RayDetector : MonoBehaviour {
 	private void DrawRay(Ray ray)
     {
 		Debug.DrawRay(ray.origin, ray.direction * RayDistance, Color.red);
+    }
+
+	private void UpdateMovement()
+    {
+		if (Input.GetKey(KeyCode.UpArrow))
+        {
+			Vector3 direction = transform.TransformPoint(Vector3.forward) - transform.position;
+			Vector3 movement = direction * Speed * Time.fixedDeltaTime;
+			transform.position += movement;
+		}
+
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			Vector3 direction = transform.TransformPoint(Vector3.back) - transform.position;
+			Vector3 movement = direction * Speed * Time.fixedDeltaTime;
+			transform.position += movement;
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+        }
     }
 }
