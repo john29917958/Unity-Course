cf81bbbfd8c7ee2e75ff6f126ef7d7a454eb30f6
Tzu-Chao Wang <john29917958@gmail.com>
2021-03-21 15:47:29 +0800

    Temp for dynamic joystick

--- src/Unity/VirtualJoystick/Assets/Scripts/Manager.cs
+++ src/Unity/VirtualJoystick/Assets/Scripts/Manager.cs
@@ -15,6 +15,20 @@ public class Manager : MonoBehaviour
 		Joystick.OnDragEnded += delegate () { _isMoving = false; };
 	}
 
+	private void Update()
+    {
+		if (Input.GetMouseButtonDown(0))
+        {
+			Joystick.gameObject.SetActive(true);
+			Joystick.transform.position = Input.mousePosition;
+        }
+
+		if (Input.GetMouseButtonUp(0))
+        {
+			Joystick.gameObject.SetActive(false);
+		}
+    }
+
 	private void FixedUpdate()
     {
 		if (_isMoving) MoveCharacter(Joystick.Direction);
