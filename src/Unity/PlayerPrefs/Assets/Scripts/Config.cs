a17ed64f9b7c7f8a91dfcc518fb489c56d131830
Tzu-Chao Wang <john29917958@gmail.com>
2021-03-21 10:27:44 +0800

    Re-organizes folder structure

---
+++ src/Unity/PlayerPrefs/Assets/Scripts/Config.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Config
+{
+    public string Name;
+    public int Level;
+    public float Experience;    
+
+    public void Save()
+    {
+        string configData = JsonUtility.ToJson(this);
+        PlayerPrefs.SetString("config", configData);
+        PlayerPrefs.Save();
+    }
+
+    public void Refresh()
+    {
+        Config reloadedConfig = Load();
+        Name = reloadedConfig.Name;
+        Level = reloadedConfig.Level;
+        Experience = reloadedConfig.Experience;
+    }
+
+    public static Config Load()
+    {
+        string configData = PlayerPrefs.GetString("config");
+        Config config = JsonUtility.FromJson<Config>(configData);
+        if (config == null) config = new Config();
+        return config;
+    }
+}
