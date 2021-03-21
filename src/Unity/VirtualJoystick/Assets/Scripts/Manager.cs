14f2f3c79b8f1729071fc708a3f8b65291bf53b4
Tzu-Chao Wang <john29917958@gmail.com>
2021-03-21 15:33:49 +0800

    Adds virtual joystick project

---
+++ src/Unity/VirtualJoystick/Assets/Scripts/Manager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+	public float MoveSpeed;
+	public VirtualJoystick Joystick;
+	public Image Character;
+
+	private bool _isMoving;
+	
+	private void Start()
+    {
+		Joystick.OnDragStarted += delegate () { _isMoving = true; };
+		Joystick.OnDragEnded += delegate () { _isMoving = false; };
+	}
+
+	private void FixedUpdate()
+    {
+		if (_isMoving) MoveCharacter(Joystick.Direction);
+    }
+
+	private void MoveCharacter(Vector2 direction)
+    {
+		Character.transform.position += Vector3.ClampMagnitude(new Vector3(direction.x, direction.y, 0), MoveSpeed);
+	}
+}
---
+++ src/Unity/VirtualJoystick/Assets/Scripts/VirtualJoystick.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class VirtualJoystick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Vector2 Direction { get; private set; }
+    public Action OnDragStarted;    
+    public Action OnDragEnded;
+
+    public RectTransform JoyStickArea;
+    public RectTransform Handler;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (OnDragStarted != null) OnDragStarted.Invoke();
+        Direction = GetDragDirection(eventData.position);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        SetHandlerPosition(eventData.position);
+        Direction = GetDragDirection(eventData.position);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Handler.anchoredPosition = Vector2.zero;
+        Direction = Vector2.zero;
+        if (OnDragEnded != null) OnDragEnded.Invoke();
+    }
+
+    private void SetHandlerPosition(Vector2 pointerPosition)
+    {
+        Vector2 origin = new Vector2(JoyStickArea.transform.position.x, JoyStickArea.transform.position.y);
+        Vector2 diff = pointerPosition - origin;
+        float radius = JoyStickArea.rect.width / 2f;
+
+        if (diff.magnitude > radius)
+        {
+            diff = Vector2.ClampMagnitude(diff, radius);
+        }
+
+        Handler.anchoredPosition = diff;
+    }
+
+    private Vector2 GetDragDirection(Vector2 pointerPosition)
+    {
+        Vector2 origin = new Vector2(JoyStickArea.transform.position.x, JoyStickArea.transform.position.y);
+        Vector2 diff = pointerPosition - origin;
+        Vector2 direction = diff.normalized;
+        return direction;
+    }
+}
