ec54c9e22f9d0cfbaf2179f5f72479d69af269c3
Tzu-Chao Wang <john29917958@gmail.com>
2021-03-28 14:49:47 +0800

    Adds terrain project

---
+++ src/Unity/Terrain/Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private Rigidbody _body;
+
+    private void Update()
+    {
+        Vector3 frontBackVelocity = Vector3.zero;
+        Vector3 leftRightVelocity = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            frontBackVelocity = transform.forward;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            frontBackVelocity = transform.forward * -1;
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            leftRightVelocity = transform.right * -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            leftRightVelocity = transform.right;
+        }
+
+        Vector3 velocity = (frontBackVelocity + leftRightVelocity).normalized * _speed;
+
+        _body.velocity = velocity;
+    }
+}
