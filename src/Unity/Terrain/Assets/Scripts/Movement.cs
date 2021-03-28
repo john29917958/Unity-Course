8318c3d3fdbeeac79fc042436cc61689c2f64a66
Tzu-Chao Wang <john29917958@gmail.com>
2021-03-28 16:51:32 +0800

    Corrects movement

--- src/Unity/Terrain/Assets/Scripts/Movement.cs
+++ src/Unity/Terrain/Assets/Scripts/Movement.cs
@@ -9,16 +9,17 @@ public class Movement : MonoBehaviour
     private float _rotationSpeed;
 
     [SerializeField]
-    private GameObject _headV;
+    private GameObject _player;
 
     [SerializeField]
-    private GameObject _headH;
+    private GameObject _head;
 
     [SerializeField]
     private Rigidbody _body;
 
     private void FixedUpdate()
     {
+        //Vector3 direction = transform.rotation.eulerAngles.normalized;
         Vector3 frontBackVelocity = Vector3.zero;
         Vector3 leftRightVelocity = Vector3.zero;
 
@@ -40,7 +41,7 @@ public class Movement : MonoBehaviour
             leftRightVelocity = transform.right;
         }
 
-        Vector3 velocity = (frontBackVelocity + leftRightVelocity).normalized * _moveSpeed;
+        Vector3 velocity = (frontBackVelocity + leftRightVelocity).normalized * _moveSpeed * Time.fixedDeltaTime;
 
         _body.velocity = velocity;        
     }
@@ -50,23 +51,25 @@ public class Movement : MonoBehaviour
         if (Input.GetAxis("Mouse X") > 0.1f)
         {
             // Mouse moves right
-            _headH.transform.Rotate(Vector3.up, _rotationSpeed);
+            _player.transform.Rotate(Vector3.up, _rotationSpeed * Time.deltaTime);
         }
         else if (Input.GetAxis("Mouse X") < -0.1f)
         {
             // Mouse moves left
-            _headH.transform.Rotate(Vector3.down, _rotationSpeed);
-        }        
+            _player.transform.Rotate(Vector3.down, _rotationSpeed * Time.deltaTime);
+        }
 
-        if (Input.GetAxis("Mouse Y") > 0.1f)
+        Debug.Log(Mathf.Abs(_head.transform.rotation.x));
+
+        if (Input.GetAxis("Mouse Y") > 0.1f && _head.transform.rotation.x >= -0.3f)
         {
             // Mouse moves up
-            _headV.transform.Rotate(Vector3.left, _rotationSpeed);
+            _head.transform.Rotate(Vector3.left, _rotationSpeed * Time.deltaTime);
         }
-        else if (Input.GetAxis("Mouse Y") < -0.1f)
+        else if (Input.GetAxis("Mouse Y") < -0.1f && _head.transform.rotation.x <= 0.3f)
         {
             // Mouse moves down
-            _headV.transform.Rotate(Vector3.right, _rotationSpeed);
+            _head.transform.Rotate(Vector3.right, _rotationSpeed * Time.deltaTime);
         }
     }
 }
