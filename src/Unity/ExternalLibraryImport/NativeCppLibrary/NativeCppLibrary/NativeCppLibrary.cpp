30f9f58475e59b9ce306498ab0e0d381137181b1
MarcusChen <marcus852520@gmail.com>
2021-04-11 13:20:53 +0800

    Adds C++ project

---
+++ src/Unity/ExternalLibraryImport/NativeCppLibrary/NativeCppLibrary/NativeCppLibrary.cpp
@@ -0,0 +1,38 @@
+﻿// NativeCppLibrary.cpp : 定義 DLL 的匯出函式。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "NativeCppLibrary.h"
+
+
+// 這是匯出變數的範例
+NATIVECPPLIBRARY_API int nNativeCppLibrary=0;
+
+// 這是匯出函式的範例。
+NATIVECPPLIBRARY_API int fnNativeCppLibrary(void)
+{
+    return 0;
+}
+
+// 這是已匯出的類別建構函式。
+CNativeCppLibrary::CNativeCppLibrary()
+{
+    return;
+}
+
+int displayNumber() {
+    return 1;
+}
+
+int getRandom() {
+    return 10;
+}
+
+int displaySum()
+{
+    int first_number = 7;
+    int second_number = 7;
+    int total = first_number + second_number;
+    return total;
+}
---
+++ src/Unity/ExternalLibraryImport/NativeCppLibrary/NativeCppLibrary/NativeCppLibrary.h
@@ -0,0 +1,29 @@
+﻿// 下列 ifdef 區塊是建立巨集的標準方式，讓從 DLL 匯出的過程更簡單。
+// 此 DLL 中的所有檔案均使用命令列中定義的 NATIVECPPLIBRARY_EXPORTS 來編譯。
+// 此符號不應在其他使用此 DLL 的任何專案中定義
+// 這樣一來，原始程式檔中包含此檔案的其他任何專案，
+// 都會將 NATIVECPPLIBRARY_API 函式視為從 DLL 匯入，
+// 而此 DLL 會將使用此巨集定義的符號視為匯出。
+#ifdef NATIVECPPLIBRARY_EXPORTS
+#define NATIVECPPLIBRARY_API __declspec(dllexport)
+#else
+#define NATIVECPPLIBRARY_API __declspec(dllimport)
+#endif
+
+// 這個類別是從 dll 匯出的
+class NATIVECPPLIBRARY_API CNativeCppLibrary {
+public:
+	CNativeCppLibrary(void);
+	// TODO: 在此新增您的方法。
+};
+
+extern NATIVECPPLIBRARY_API int nNativeCppLibrary;
+
+NATIVECPPLIBRARY_API int fnNativeCppLibrary(void);
+
+extern "C"
+{
+	NATIVECPPLIBRARY_API int displayNumber();
+	NATIVECPPLIBRARY_API int getRandom();
+	NATIVECPPLIBRARY_API int displaySum();
+}
