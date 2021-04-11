2fd4d74bab0915b919232be1f6f9d6c1be65b7f4
MarcusChen <marcus852520@gmail.com>
2021-04-11 14:34:30 +0800

    Adds asset bundle project

---
+++ src/Unity/AssetBundle/Assets/Scripts/AssetBundleMaker.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetBundleMaker : Editor
+{
+    [MenuItem("AssetBundles/Make")]
+    private static void Make()
+    {
+        string bundleFolder = "AssetBundles";
+        if (!Directory.Exists(bundleFolder))
+        {
+            Directory.CreateDirectory(bundleFolder);
+        }
+
+        BuildPipeline.BuildAssetBundles(bundleFolder, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+        Debug.Log("Asset bundle maked");
+    }
+}
---
+++ src/Unity/AssetBundle/Assets/Scripts/Movement.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed;
+
+    [SerializeField]
+    private float _rotationSpeed;
+
+    [SerializeField]
+    private float _sprintSpeed;
+
+    [SerializeField]
+    private GameObject _player;
+
+    [SerializeField]
+    private GameObject _head;
+
+    [SerializeField]
+    private Rigidbody _body;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _body.AddRelativeForce(Vector3.forward * _sprintSpeed, ForceMode.Impulse);
+        }
+
+        Vector3 frontBackVelocity = Vector3.zero;
+        Vector3 leftRightVelocity = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            frontBackVelocity = transform.forward;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            frontBackVelocity = transform.forward * -1;
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            leftRightVelocity = transform.right * -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            leftRightVelocity = transform.right;
+        }
+
+        Vector3 velocity = (frontBackVelocity + leftRightVelocity).normalized * _moveSpeed;
+
+        if (velocity.magnitude > 0)
+        {            
+            _body.velocity = velocity;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Mouse X") > 0.1f)
+        {
+            // Mouse moves right
+            _player.transform.Rotate(Vector3.up, _rotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetAxis("Mouse X") < -0.1f)
+        {
+            // Mouse moves left
+            _player.transform.Rotate(Vector3.down, _rotationSpeed * Time.deltaTime);
+        }
+
+        Debug.Log(Mathf.Abs(_head.transform.rotation.x));
+
+        if (Input.GetAxis("Mouse Y") > 0.1f && _head.transform.rotation.x >= -0.3f)
+        {
+            // Mouse moves up
+            _head.transform.Rotate(Vector3.left, _rotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetAxis("Mouse Y") < -0.1f && _head.transform.rotation.x <= 0.3f)
+        {
+            // Mouse moves down
+            _head.transform.Rotate(Vector3.right, _rotationSpeed * Time.deltaTime);
+        }
+    }
+}
