75c468775b6c03c72f0cc639810d46bf962cb7db
MarcusChen <marcus852520@gmail.com>
2021-04-11 14:48:17 +0800

    Updates bundle maker

--- src/Unity/AssetBundle/Assets/Scripts/AssetBundleMaker.cs
+++ src/Unity/AssetBundle/Assets/Scripts/AssetBundleMaker.cs
@@ -1,19 +1,21 @@
-﻿using System.IO;
+﻿#if UNITY_EDITOR
 using UnityEditor;
+using System.IO;
 using UnityEngine;
 
 public class AssetBundleMaker : Editor
 {
     [MenuItem("AssetBundles/Make")]
-    private static void Make()
+    static void Make()
     {
-        string bundleFolder = "AssetBundles";
+        string bundleFolder = Path.Combine(Path.GetTempPath(), "AssetBundles");
         if (!Directory.Exists(bundleFolder))
         {
             Directory.CreateDirectory(bundleFolder);
         }
 
         BuildPipeline.BuildAssetBundles(bundleFolder, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
-        Debug.Log("Asset bundle maked");
+        Debug.Log("Asset bundle maked in \"" + bundleFolder + '"');
     }
 }
+#endif
