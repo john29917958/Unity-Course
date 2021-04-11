880ea10fb22b3db33bc143fd6c8ad4e22c557680
MarcusChen <marcus852520@gmail.com>
2021-04-11 13:14:44 +0800

    Adds native C++ plugin project

---
+++ src/Unity/ExternalLibraryImport/Unity/New Unity Project/Assets/Scripts/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+	[DllImport("NATIVECPPLIBRARY", EntryPoint = "displayNumber")]
+	public static extern int displayNumber();
+
+	[DllImport("NATIVECPPLIBRARY", EntryPoint = "getRandom")]
+	public static extern int getRandom();
+
+	[DllImport("NATIVECPPLIBRARY", EntryPoint = "displaySum")]
+	public static extern int displaySum();
+
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log(displayNumber());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
