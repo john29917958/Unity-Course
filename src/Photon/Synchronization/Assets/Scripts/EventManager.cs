a2e292a9119ab2611a8d2bc8f3ff828cdffaae37
Tzu-Chao Wang <john29917958@gmail.com>
2021-05-30 12:16:51 +0800

    Completes basic Photon View synchronization

---
+++ src/Photon/Synchronization/Assets/Scripts/EventManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public GameObject PhotonObject;
+
+    private List<GameObject> _objectPool;
+
+    private void Awake()
+    {
+        _objectPool = new List<GameObject>();
+    }
+
+    public void CreateCube()
+    {
+        GameObject obj = PhotonNetwork.Instantiate(PhotonObject.name, Vector3.zero, Quaternion.identity, 0, new object[] {"Hi", 123});
+        _objectPool.Add(obj);
+    }
+
+    public void CleanCubes()
+    {
+        Debug.Log("Before clean: " + _objectPool.Count);
+        foreach (GameObject obj in _objectPool.ToArray())
+        {
+            PhotonNetwork.Destroy(obj);
+            _objectPool.Remove(obj);
+        }
+        Debug.Log("After clean: " + _objectPool.Count);
+    }
+}
