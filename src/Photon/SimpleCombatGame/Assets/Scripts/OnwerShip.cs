7c7dcf9789f95f05759cf8964f785c2d98db5730
Tzu-Chao Wang <john29917958@gmail.com>
2021-06-06 11:42:38 +0800

    Adds ownership taking script

---
+++ src/Photon/SimpleCombatGame/Assets/Scripts/OnwerShip.cs
@@ -0,0 +1,32 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class OnwerShip : MonoBehaviour, IPunOwnershipCallbacks
+{
+    [SerializeField] private PhotonView _view;
+
+    public void RequestOwnership()
+    {
+        Debug.Log("Taking ownership");
+        _view.RequestOwnership();
+    }
+
+    public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
+    {
+        if (true)
+        {
+            targetView.TransferOwnership(requestingPlayer);
+        }
+    }
+
+    public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
+    {
+        Debug.Log("Ownership transferred to: " + PhotonNetwork.LocalPlayer.NickName);
+    }
+
+    public void OnOwnershipTransferFailed(PhotonView targetView, Player senderOfFailedRequest)
+    {
+        Debug.Log(senderOfFailedRequest.NickName + " failed to take ownership");
+    }
+}
